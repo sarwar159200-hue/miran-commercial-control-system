26824e8ad62aeea84cf623aef6c3f698650c96ea
sarwar159200-hue <sarwar.159200@gmail.com>
2026-08-24 14:58:54 +0300

    Delete MCCS-V2.9.1-REPO-ROOT-READY directory

--- MCCS-V2.9.1-REPO-ROOT-READY/app/(auth)/login/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function LoginLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return children;
-}
